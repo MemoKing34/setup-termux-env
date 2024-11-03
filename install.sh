10baf0197336cab1dcd7d4dc0d0597a0e41665a1
MemoKing34 <karam24@itu.edu.tr>
2024-11-03 17:41:32 +0300

    setup upgraded

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pkg install git -y
-git clone https://github.com/MemoKing34/setup-termux-env
+git clone https://github.com/MemoKing34/setup-termux-env.git
 cd setup-termux-env
 bash scripts/setup-termux-env.sh
