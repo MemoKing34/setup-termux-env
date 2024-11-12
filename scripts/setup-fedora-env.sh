035a03f63aea48e81119da639cc6b9a21ddcf62f
MemoKing34 <karam24@itu.edu.tr>
2024-11-12 11:44:24 +0300

    fix

--- scripts/setup-fedora-env.sh
+++ scripts/setup-fedora-env.sh
@@ -5,13 +5,13 @@ sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfre
 sudo dnf update -y
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf groupupdate sound-and-video
-sudo dnf install neofetch git kde-partitionmanager tlp tlp-rdw snapd zsh -y
+sudo dnf install neofetch git kde-partitionmanager tlp tlp-rdw snapd zsh -y --skip-unavailable
 sudo firewall-cmd --add-service=ssh --permanent
 sudo firewall-cmd --reload
 sudo systemctl enable --now  tlp
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
-sudo snap install code thunderbird pycharm-community -y
+sudo snap install code thunderbird pycharm-community 
 flatpak install flathub io.github.zen_browser.zen
 
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/MemoKing34/setup-termux-env/refs/heads/master/scripts/setup.zsh)"
