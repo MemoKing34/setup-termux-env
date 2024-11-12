33032dfef394b8c9c01dd84791d6a97e1c92456a
MemoKing34 <karam24@itu.edu.tr>
2024-11-12 11:42:04 +0300

    added fedora setup

--- README.md
+++ README.md
@@ -14,5 +14,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/MemoKing34/setup-termux-en
 
 if you want to apply only zsh scripts run `scripts/setup.zsh` or this script
 ```bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/MemoKing34/setup-termux-env/refs/heads/master/scripts/setup.zsh)"
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/MemoKing34/setup-termux-env/refs/heads/master/scripts/setup.zsh)"
+```
+
+Now i added fedora setup it will install some packages for you
+
+- If you want to install asus drivers [click here](https://asus-linux.org/guides/fedora-guide/)
+
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/MemoKing34/setup-termux-env/refs/heads/master/scripts/setup-fedora-env.sh)"
 ```
---
+++ scripts/setup-fedora-env.sh
@@ -0,0 +1,17 @@
+sudo dnf update -y
+sudo dnf groupinstall "KDE Applications"
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf update -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video
+sudo dnf install neofetch git kde-partitionmanager tlp tlp-rdw snapd zsh -y
+sudo firewall-cmd --add-service=ssh --permanent
+sudo firewall-cmd --reload
+sudo systemctl enable --now  tlp
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install code thunderbird pycharm-community -y
+flatpak install flathub io.github.zen_browser.zen
+
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/MemoKing34/setup-termux-env/refs/heads/master/scripts/setup.zsh)"
