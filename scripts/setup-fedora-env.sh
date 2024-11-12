c70f7330dbf5c57840e828ee178179d0e3c6468d
MemoKing34 <karam24@itu.edu.tr>
2024-11-12 11:57:26 +0300

    push

--- scripts/setup-fedora-env.sh
+++ scripts/setup-fedora-env.sh
@@ -1,17 +1,33 @@
+read -p "Active flathub from your store app and hit enter"
+
+
+echo 'Running dnf update, will ask sudo password'
 sudo dnf update -y
+
+echo 'Installing KDE'
 sudo dnf groupinstall "KDE Applications"
+
+sudo 'Activating RPM Fusion repos'
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update -y
+
+echo 'Some sound staff'
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf groupupdate sound-and-video
-sudo dnf install neofetch git kde-partitionmanager tlp tlp-rdw snapd zsh -y --skip-unavailable
-sudo firewall-cmd --add-service=ssh --permanent
-sudo firewall-cmd --reload
+
+echo 'Installing additional packages'
+sudo dnf install neofetch git kde-partitionmanager tlp tlp-rdw snapd zsh -y --skip-unavailable --skip-broken
+
+echo 'Enabling  tlp and snap services'
 sudo systemctl enable --now  tlp
 sudo systemctl enable --now snapd.socket
+
+echo 'Setting snap'
 sudo ln -s /var/lib/snapd/snap /
-sudo snap install code thunderbird pycharm-community 
+echo 'Installing additional snap packages'
+sudo snap install code thunderbird pycharm-community
+echo 'Installing zen browser from flathub' 
 flatpak install flathub io.github.zen_browser.zen
 
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/MemoKing34/setup-termux-env/refs/heads/master/scripts/setup.zsh)"
--- scripts/setup.zsh
+++ scripts/setup.zsh
@@ -18,7 +18,7 @@ echo 'Installing powerlevel10k'
 if [[ $(test -d $THEMES/powerlevel10k) ]]; then
     echo 'Powerlevel10k already installed!'
 else
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $THEMES/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $THEMES/powerlevel10k -q
     echo 'Powerlevel10k installed'
 fi
 echo 'Setting p10k dotfiles'
@@ -34,9 +34,9 @@ fi
 
 
 
-echo 'Installing extra plugins for oh my zsh'
-test -d "$PLUGINS/zsh-autosuggestions" || git clone https://github.com/zsh-users/zsh-autosuggestions $PLUGINS/zsh-autosuggestions
-test -d "$PLUGINS/zsh-syntax-highlighting" || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PLUGINS/zsh-syntax-highlighting
-test -d "$PLUGINS/you-should-use" || git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $PLUGINS/you-should-use
-test -d "$PLUGINS/zsh-bat" || git clone https://github.com/fdellwing/zsh-bat.git $PLUGINS/zsh-bat
+echo 'Installing extra plugins for oh my zsh (zsh-autosuggestions zsh-syntax-highlighting you-should-use zsh-bat)'
+test -d "$PLUGINS/zsh-autosuggestions" || git clone https://github.com/zsh-users/zsh-autosuggestions $PLUGINS/zsh-autosuggestions -q
+test -d "$PLUGINS/zsh-syntax-highlighting" || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PLUGINS/zsh-syntax-highlighting -q
+test -d "$PLUGINS/you-should-use" || git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $PLUGINS/you-should-use -q
+test -d "$PLUGINS/zsh-bat" || git clone https://github.com/fdellwing/zsh-bat.git $PLUGINS/zsh-bat -q
 echo 'Extra plugins installed'
